02622d5d6a303a3bb29aa5af7be39cbb7240202f
Xzavier Dunn <xmandunn@gmail.com>
2023-04-03 09:01:17 -0400

    fixed hello run

--- src/commands/hello.rs
+++ src/commands/hello.rs
@@ -1,6 +1,9 @@
 use serenity::builder::CreateApplicationCommand;
 use serenity::model::prelude::command::CommandOptionType;
-use serenity::model::prelude::interaction::application_command::CommandDataOption;
+use serenity::model::prelude::interaction::application_command::{
+    CommandDataOption, CommandDataOptionValue,
+};
+use serenity::model::user::User;
 use serenity::utils::MessageBuilder;
 
 pub fn run(options: &[CommandDataOption]) -> String {
@@ -8,18 +11,36 @@ pub fn run(options: &[CommandDataOption]) -> String {
         return "Got incorrect number of options".to_string();
     }
 
-    let user = &options.get(0);
+    let user_option = options
+        .get(0)
+        .expect("Expected user option")
+        .resolved
+        .as_ref()
+        .expect("Expected User object");
 
-    println!("Before Data:");
-    println!("{:?}", user);
-    // println!("{:?}", message);
+    let msg_option = options
+        .get(1)
+        .expect("Expected message option")
+        .resolved
+        .as_ref()
+        .expect("Expected string object");
 
-    // return format!("{:?} {:?}", message, user);
+    let user: &User;
+    let msg: String;
 
-    MessageBuilder::new()
-        .push("testing")
-        // .mention(user)
-        .build()
+    if let CommandDataOptionValue::User(user_obj, _) = user_option {
+        user = user_obj;
+    } else {
+        return "Missing User".to_string();
+    }
+
+    if let CommandDataOptionValue::String(msg_string) = msg_option {
+        msg = format!("{}\t", msg_string);
+    } else {
+        return "Missing Message".to_string();
+    }
+
+    return MessageBuilder::new().push(msg).mention(user).build();
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
